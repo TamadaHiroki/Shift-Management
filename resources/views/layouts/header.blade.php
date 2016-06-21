c6a62c271a9b4d808ff62de91121942b758b6ffb
中川 直哉 <2130152@maetel.ecc.ac.jp>
2016-06-21 14:25:59 +0900

    bladeでテンプレート化

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,30 @@
+<div class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <!-- Logo -->
+                <div class="logo">
+                    <h1><a href="main.html">シフト管理(β) @yield('title')</a></h1>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="row">
+                    <div class="col-lg-12">
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="navbar navbar-inverse" role="banner">
+                    <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
+                        <ul class="nav navbar-nav">
+                            <a href="logout.html">ログアウト </a>
+
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
