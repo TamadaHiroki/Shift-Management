848152b51741b03870630622c5ae1ce085183638
川﨑 翔太 <2130041@maetel.ecc.ac.jp>
2016-06-27 12:26:56 +0900

    シフト確認画面

---
+++ resources/views/sift.blade.php
@@ -0,0 +1,118 @@
+@extends('layouts.master)
+
+<div class="page-content">
+    <div class="row">
+        <div class="col-md-2">
+            <div class="sidebar content-box" style="display: block;">
+                <ul class="nav">
+                    <!-- Main menu -->
+
+                    <!--�T�C�h�o�[-->
+                    <li class="current"><a href="index.html"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
+                    <li><a href="calendar.html"><i class="glyphicon glyphicon-calendar"></i> Calendar</a></li>
+                    <li><a href="stats.html"><i class="glyphicon glyphicon-stats"></i> Statistics (Charts)</a></li>
+                </ul>
+                <!--���T�C�h�o�[�I���-->
+            </div>
+        </div>
+        <div class="col-md-10">
+            <div class="row">
+
+
+                <!---���C���̉��--->
+                <!-- styles -->
+                <link href="css/buttons.css" rel="stylesheet">
+                &nbsp
+                &nbsp
+                &nbsp
+                <button  class="btn btn-info btn-lg">��T</button>
+                <button class="btn btn-info btn-lg">���T</button>
+                <button class="text-right bg -danger"></button>
+                <button class="btn btn-info btn-lg">�߂�</button>
+
+                </span>
+                <br>
+                <div class="col-md-6">
+                    <div class="content-box-large">
+                        <div class="panel-heading">
+                            <div class="panel-title">Table with row classes</div>
+
+                            <div class="panel-options">
+                                <a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
+                                <a href="#" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            <table class="table">
+                                <thead>
+                                <!-- <tr>
+                                   <th>#</th>
+                                   <th>First Name</th>
+                                   <th>Last Name</th>
+                                   <th>Username</th>
+                                 </tr> -->
+                                <tr align="center">
+                                    <td>1-1</td>
+                                    <td>1-2</td>
+                                    <th><var week get Dey()></th>
+                                    <td>1-3</td>
+                                    <td>1-4</td>
+                                    <td>1-5</td>
+                                    <td>1-6</td>
+                                    <td>1-7</td>
+                                    <td>1-8</td>
+                                    <td>1-9</td>
+                                </tr>
+                                <tr align="center">
+                                    <td>2-1</td>
+                                    <td>2-2</td>
+                                    <td>2-3</td>
+                                    <td>2-4</td>
+                                    <td>2-5</td>
+                                    <td>2-6</td>
+                                    <td>2-7</td>
+                                    <td>2-8</td>
+                                    <td>2-9</td>
+                                </tr>
+                                <tr align="center">
+                                    <td>3-1</td>
+                                    <td>3-2</td>
+                                    <td>3-3</td>
+                                    <td>3-4</td>
+                                    <td>3-5</td>
+                                    <td>3-6</td>
+                                    <td>3-7</td>
+                                    <td>3-8</td>
+                                    <td>3-9</td>
+                                </tr>
+                                <tr align="center">
+                                    <td>4-1</td>
+                                    <td>4-2</td>
+                                    <td>4-3</td>
+                                    <td>4-4</td>
+                                    <td>4-5</td>
+                                    <td>4-6</td>
+                                    <td>4-7</td>
+                                    <td>4-8</td>
+                                    <td>4-9</td>
+                                </tr>
+
+                                </thead>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!---->
+
+
+
+        </div>
+    </div>
+</div>
+
+
+</div>
+</div>
+</div>
