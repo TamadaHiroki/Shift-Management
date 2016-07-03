a880e82de2f24d0b5001807b2f1017b4231a7dae
Tamada Hiroki <rrhayeaerra-exechymmnosphantasmagoriaanyora@yahoo.co.jp>
2016-07-03 19:56:49 +0900

    レイアウト用のblade

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,11 @@
+<div class="col-md-2">
+    <div class="sidebar content-box" style="display: block;">
+        <ul class="nav">
+            <!-- Main menu -->
+            <li class="current"><a href="main"><i class="glyphicon glyphicon-home"></i> ホーム</a></li>
+            <li><a href="a"><i class="glyphicon glyphicon-calendar"></i> [空]シフト表示</a></li>
+            <li><a href="b"><i class="glyphicon glyphicon-refresh"></i> [空]パスワード変更</a></li>
+
+        </ul>
+    </div>
+</div>
---
+++ resources/views/user/layouts/header.blade.php
@@ -0,0 +1,34 @@
+
+
+<div class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <!-- Logo -->
+                <div class="logo">
+                    <h1> <font color="white">店舗管理 @yield('title') </font></h1>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="row">
+                    <div class="col-lg-12">
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="navbar navbar-inverse" role="banner">
+                    <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
+                        <ul class="nav navbar-nav">
+
+                            <button type="button" onclick="location.href='logout'" class="btn btn-lg btn-block btn-success">ログアウト</button>
+                            <!--<a href="logout.html"><font color="white">ログアウト</font> </a>-->
+
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
