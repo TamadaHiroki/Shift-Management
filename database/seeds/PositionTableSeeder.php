3409049e1bba554c8a1e64a241b344220bbd7fea
山田 誠志 <2130122@maetel.ecc.ac.jp>
2016-07-19 15:35:42 +0900

    PositionTableのseeder修正とseederの追加

--- database/seeds/PositionTableSeeder.php
+++ database/seeds/PositionTableSeeder.php
@@ -15,15 +15,23 @@ class PositionTableSeeder extends Seeder
 
         DB::table('position')->insert([
             'position' => 'レジ',
+            'start_time' => '09:00:00',
+            'end_time' => '22:00:00',
         ]);
         DB::table('position')->insert([
             'position' => '惣菜',
+            'start_time' => '09:00:00',
+            'end_time' => '22:00:00',
         ]);
         DB::table('position')->insert([
             'position' => '鮮魚',
+            'start_time' => '09:00:00',
+            'end_time' => '22:00:00',
         ]);
         DB::table('position')->insert([
             'position' => '商品補充',
+            'start_time' => '09:00:00',
+            'end_time' => '22:00:00',
         ]);
     }
 }
